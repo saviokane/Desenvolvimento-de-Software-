1670dc959649995930871b86a5b085595c9be9f1
Savio Kane <savio.kanesk@gmail.com>
2023-03-27 01:14:49 -0300

    Correção exe13

--- Lista02/src/kane/savio/util/exe13.java
+++ Lista02/src/kane/savio/util/exe13.java
@@ -11,17 +11,29 @@ public static void main(String[] args) {
 	n = leitor.nextInt();
 	
 	int[] fib = new int[n];
-	
+	int a = 0;
 	fib[0] = 0;
+	if(n==1) {
+		System.out.println(fib[0]);
+		
+	}
 		if(n > 1) {
 			fib[1] = 1;
-		}
-	
-		for (int i = 0; i < n; i++) {
-			int menos1 = -1, menos2 = -2;
-			fib[i] = fib[i] + (fib[i-menos1] + fib[i-menos2]);
+			System.out.println(fib[0]);
+			System.out.println(fib[1]);
+			
+			
+			
+		
+	// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584
+		for (int i = 2; i < n; i++) {
+			
+//			int menos1 = -1, menos2 = -2;
+			fib[i] = fib[i] + (fib[i-1] + fib[i-2]);
+			
 			System.out.println(fib[i]);
 		}
-	// desisto desse aqui
+		}
+	
 	}
 }
--- Lista02/src/kane/savio/util/exe14.java
+++ Lista02/src/kane/savio/util/exe14.java
@@ -1,5 +1,24 @@
-package kane.savio.util;
+import java.util.Scanner;
 
 public class exe14 {
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o valor de n: ");
+        int n = input.nextInt();
+        input.close();
+        
+        int[] fibonacci = new int[n];
+        fibonacci[0] = 1;
+        if (n > 1) {
+            fibonacci[1] = 1;
+            for (int i = 2; i < n; i++) {
+                fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+            }
+        }
+        
+        System.out.print("Sequência de Fibonacci: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci[i] + " ");
+        }
+    }
 }
