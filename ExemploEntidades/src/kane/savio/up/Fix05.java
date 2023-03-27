cc935394df09bf98355e05d2bcb238068adb7329
Savio Kane <savio.kanesk@gmail.com>
2023-03-27 20:44:05 -0300

    Fix 07

--- ExemploEntidades/src/kane/savio/up/Fix05.java
+++ ExemploEntidades/src/kane/savio/up/Fix05.java
@@ -10,13 +10,18 @@ public class Fix05 {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		String arquivoOrigem = "C:\\_src\\ws\\kane\\ExemploEntidades\\src\\kane\\savio\\up\\entidade\\grupos-tabulados.txt";
+		String arquivoOrigem = "C:\\_src\\Desenvolvimento-de-Software-\\ExemploEntidades\\src\\kane\\savio\\up\\entidade\\grupos-tabulados.txt";
+		/* NA FACULDADE C:\\_src\\ws\\kane\\ExemploEntidades\\src\\kane\\savio\\up\\entidade\\grupos-tabulados.txt
+		 NO SETUP DE CASA É ESSE URL C:\_src\Desenvolvimento-de-Software-\ExemploEntidades\src\kane\savio\\entidade\grupos-tabulados.txt*/
 		File arquivo = new File(arquivoOrigem);
 		Scanner leitor = new Scanner(arquivo);
 		
-		String arquivoDestino = "C:\\_src\\ws\\kane\\ExemploEntidades\\src\\kane\\savio\\up\\entidade\\arquivo-destino.txt";
+		String arquivoDestino = "C:\\_src\\Desenvolvimento-de-Software-\\ExemploEntidades\\src\\kane\\savio\\up\\entidade\\arquivo-destino.txt";
 		Formatter gravador = new Formatter(arquivoDestino);
 		
+		String relatorioGrupo = "C:\\_src\\Desenvolvimento-de-Software-\\ExemploEntidades\\src\\kane\\savio\\up\\entidade\\relatorio-de-grupo.txt";
+		File arquivo1 = new File(relatorioGrupo);
+		Formatter gravador2 = new Formatter(relatorioGrupo);
 		int grupoA= 0, grupoB= 0, grupoC= 0, grupoD= 0, grupoE= 0, grupoF=0;
 		int  soma = 0; 
 		leitor.nextLine();
@@ -34,11 +39,9 @@ public class Fix05 {
 			
 			int valor = Integer.parseInt(Grupos[1]);
 			
-			
 			switch(Grupos[0]) {
 			
 			case "A":
-				
 				grupoA = grupoA + valor;
 				break;
 			case "B":
@@ -61,8 +64,6 @@ public class Fix05 {
 				break;
 				
 			}
-		
-		
 		}
 		
 		gravador.format("\nO valor do grupo A:"+grupoA);
@@ -71,16 +72,21 @@ public class Fix05 {
 		gravador.format("\nO valor do grupo D:"+grupoD);
 		gravador.format("\nO valor do grupo E:"+grupoE);
 		gravador.format("\nO valor do grupo F:"+grupoF);
-			
-		gravador.close();
-		System.out.println("O resultado da soma e: " + soma);
-			leitor.close();
-			
-			
-						
-
-		
+		gravador.format("\nO valor total dos registro é: "+soma);
 	
+		System.out.println("O resultado da soma e: " + soma);
+			
+			
+			gravador2.format("\nO valor total do grupo B é:"+grupoB);
+			gravador2.format("\nO valor total do grupo A é:"+grupoA);
+			gravador2.format("\nO valor total do grupo C é:"+grupoC);
+			gravador2.format("\nO valor total do grupo D é:"+grupoD);
+			gravador2.format("\nO valor total do grupo E é:"+grupoE);
+			gravador2.format("\nO valor total do grupo F é:"+grupoF);
+			gravador2.format("\nA soma de todos os grupos registrado é: "+soma);
+			gravador2.close();
+		leitor.close();
+		
 	}
 
 }
