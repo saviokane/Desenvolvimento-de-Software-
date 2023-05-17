436467e6f0bd51a97c788478d3e5ac518aba3aa2
Savio Kane <savio.kanesk@gmail.com>
2023-05-17 11:07:59 -0300

    Falta colocar orientado a objeto

--- Conteúdo Básico/JDBC/projetofunfando/src/principal/index.java
+++ Conteúdo Básico/JDBC/projetofunfando/src/principal/index.java
@@ -1,15 +1,40 @@
 package principal;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class index {
 	
 	public static void main(String[] args) throws SQLException {
-		listarPessoas();
-		Pessoa pessoa = new Pessoa("pedroso","1239123",12.3);
+		tela();
 		
-//		salvarPessoa(pessoa);
-//		inserirPessoas("1","1",1.1);
+	}
+	
+	public static void tela() {
+		try {
+		Scanner leitor = new Scanner(System.in);
+		int opcao;
+		System.out.println("Informe o que deseja: \n"
+				+ "	\n[1] LISTAR\n[2] INSERIR\n[3] ATUALIZAR\n[4] DELETAR");
+				opcao = leitor.nextInt();
+				
+				switch(opcao) {
+				case 1:
+					listarPessoas();
+					break;
+				case 2:
+					inserirPessoas();
+					break;
+				case 3: 
+					atualizarPessoas();
+					break;
+				case 4:
+					excluirPessoas();
+					break;
+				}
+		}catch(Exception erroscree) {
+			System.out.println("Erro ao demonstrar tela");
+		}
 	}
 	
 	public static void listarPessoas() {
@@ -38,12 +63,14 @@ public class index {
 		}
 		comando.close();
 		con.close();
+		
 		}catch(Exception erro) {
 			System.out.println("deu ruim!!!");
 		}
+		tela();
 	}
 	
-	public static void inserirPessoas(String nome,String cpf,Double altura) {
+	public static void inserirPessoas() throws SQLException {
 		try {
 			String url = "jdbc:mysql://localhost:3306/exemplojdbc";
 			String usuario = "root";
@@ -54,8 +81,9 @@ public class index {
 			
 			// 2. Criar o comando e executar a consulta
 			PreparedStatement comando = con.prepareStatement(sql);
-			comando.setString(1,"junincaraio");
-			comando.setString(2, "02935123");
+			
+			comando.setString(1,"saviocaraio");
+			comando.setString(2, "3213123123");
 			comando.setDouble(3, 2.1);
 			
 			comando.executeUpdate();
@@ -66,7 +94,66 @@ public class index {
 		}catch(Exception falha) {
 			System.out.println(falha);
 		}
+		tela();
 	}
+	
+	public static void atualizarPessoas()  {
+		try {
+		String url = "jdbc:mysql://localhost:3306/exemplojdbc";
+		String usuario = "root";
+		String senha = "positivo";
+		String sql = "UPDATE pessoas SET `nome` = ?, `cpf` = ?, `altura` = ? WHERE `id` = ?;";
+		Connection con = DriverManager.getConnection(url,usuario,senha);
+		PreparedStatement comando = con.prepareStatement(sql);
+		
+		comando.setString(1, "Juliuli");
+		comando.setString(2, "0183123");
+		comando.setDouble(3, 2.23);
+		
+		comando.setInt(4, 2);
+		
+		comando.executeUpdate();
+		comando.close();
+		con.close();
+		}catch(Exception err) {
+			System.out.println("Não foi encontrado o ID.");
+		}
+		tela();
+	}
+	
+	public static void excluirPessoas() {
+		
+		try {
+			
+		String url = "jdbc:mysql://localhost:3306/exemplojdbc";
+		String usuario = "root";
+		String senha = "positivo";
+		String sql = "UPDATE pessoas SET `nome` =`nome` = ?, `cpf` = ?, `altura` = ? WHERE `id` = ?;";
+		Connection con = DriverManager.getConnection(url,usuario,senha);
+		PreparedStatement comando = con.prepareStatement(sql);
+		
+		comando.setInt(1, 2);
+		comando.executeUpdate();
+		
+		System.out.println("Pessoa excluida com sucesso!!!");
+		comando.close();
+		con.close();
+		}catch(Exception error) {
+			System.out.println("Erro ao excluir meliante.");
+		}
+		tela();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	/*INSERT INTO `exemplojdbc`.`pessoas`
